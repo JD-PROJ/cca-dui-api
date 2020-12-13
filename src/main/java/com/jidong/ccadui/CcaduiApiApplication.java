fa1076e3b8846f6971d21c5e20085ad40e67ee66
권지원 <jiwonk419@11st.co.kr>
2020-12-13 15:33:43 +0900

    ccadui-api 프로젝트 생성 & travis CI yml 파일 추가

---
+++ src/main/java/com/jidong/ccadui/CcaduiApiApplication.java
@@ -0,0 +1,13 @@
+package com.jidong.ccadui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcaduiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcaduiApiApplication.class, args);
+    }
+
+}
