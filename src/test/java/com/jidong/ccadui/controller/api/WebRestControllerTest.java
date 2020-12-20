357d5199600a14bdc6da02ecc2004e34c6fa278d
권지원 <jiwonk419@11st.co.kr>
2020-12-20 18:40:13 +0900

    @RunWith 삭제

--- src/test/java/com/jidong/ccadui/controller/api/WebRestControllerTest.java
+++ src/test/java/com/jidong/ccadui/controller/api/WebRestControllerTest.java
@@ -5,13 +5,10 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import org.junit.jupiter.api.Test;
 
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class WebRestControllerTest {
 
